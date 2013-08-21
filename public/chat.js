d36a42b8b4925f8adc64a97db8ad457f85f822fc
Chung Nguyen <nguyenhongchung@gmail.com>
2013-08-22 06:17:02 +0700

    small tweak to the UI

--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,11 @@
+$(document).ready(function() {
+    $("#field").keyup(function(e) {
+        if(e.keyCode == 13) {
+            sendMessage();
+        }
+    });
+});
+
 window.onload = function() {
  
     var messages = [];
@@ -5,13 +13,15 @@ window.onload = function() {
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
+    var name = document.getElementById("name");
  
     socket.on('message', function (data) {
         if(data.message) {
-            messages.push(data.message);
+            messages.push(data);
             var html = '';
             for(var i=0; i<messages.length; i++) {
-                html += messages[i] + '<br />';
+                html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
+                html += messages[i].message + '<br />';
             }
             content.innerHTML = html;
         } else {
@@ -19,9 +29,14 @@ window.onload = function() {
         }
     });
  
-    sendButton.onclick = function() {
-        var text = field.value;
-        socket.emit('send', { message: text });
+    sendButton.onclick = sendMessage = function() {
+        if(name.value == "") {
+            alert("Please type your name!");
+        } else {
+            var text = field.value;
+            socket.emit('send', { message: text, username: name.value });
+            field.value = "";
+        }
     };
  
 }
